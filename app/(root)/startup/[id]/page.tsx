b64a14704196d9f1516ce266568a76567b075b6c
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-11-01 10:21:11 +0530

    add ppr setup

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,7 +1,18 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+export const experminetal_ppr = true;
+
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  return (
+    <>
+      <h1 className="text-3xl">Startup {post?.title}</h1>
+    </>
+  );
 };
 
 export default page;
